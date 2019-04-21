a09d31fd44d776efa2a50d370da8e96992827491
bbarnard-dg <bbarnard@digitalguardian.com>
2019-04-20 22:38:37 -0600

    Add project files.

---
+++ dgMetricsDashboard/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace dgMetricsDashboard.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
